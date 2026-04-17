010964b77ef89de556feb2f581f39b06540a4f59
MADHU8912 <nikhilabba12@gmail.com>
2026-04-17 11:16:03 +0530

    Initial bank devops project

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Bank backend running");
+});
+
+app.get("/api/accounts", (req, res) => {
+  res.json([
+    { id: 1, name: "Nikhil", balance: 5000 },
+    { id: 2, name: "Madhu", balance: 7000 }
+  ]);
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
